7a37976ffc48ca1191aba67b49c2b531660847f5
Cesar Medina <cesarmedina@Cesars-MacBook-Pro-2.local>
2022-02-16 10:00:51 -0800

    add script and modify README

--- Roza2019_GWAS.R
+++ Roza2019_GWAS.R
@@ -1,3 +1,46 @@
-# test1 
-
+rm(list = ls()) # clean Global Environment
+# setwd("~/Documents/Cesar/blup_data/Roza2019/Analysis_2021/GWAS/")
+# setwd("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/git/Roza2019/")
 library(GWASpoly)
+library(tidyverse)
+library(vcfR)
+library(parallel)
+library(doParallel)
+library(iterators)
+library(foreach)
+library(tidyr)
+library(devtools)
+library(sommer)
+library(ggplot2)
+library(ggpubr)
+library(data.table)
+library(ggthemes)
+library(hrbrthemes)
+library(VennDiagram)
+library(plotly)
+library(reticulate)
+
+load("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/GWASpoly_results/data_3_80177_year.RData")
+
+####################
+# using all mr.bean data (pheno_nph.csv) and MPP_Ms2_GWASPoly.txt
+
+setwd("~/Documents/Cesar/blup_data/Roza2019/Analysis_2021/GWAS/")
+pheno <- read.csv("~/Documents/Cesar/blup_data/Roza2019/Analysis_2021/GWAS/pheno_nph.csv", row.names = 1)
+head(pheno)
+trait1 <- colnames(pheno)[1:(length(colnames(pheno))-3)]
+trait1
+params <- set.params(fixed=c("PC1","PC2","PC3"),
+                     fixed.type=rep("numeric",3), n.PC = 3)
+models_1 <- c("general", "additive", "1-dom", "2-dom",  "diplo-additive", "diplo-general")
+# models_1 <- c("general", "additive", "1-dom",  "diplo-additive")
+data_1 <- read.GWASpoly(ploidy=4, 
+                        pheno.file="pheno_nph.csv", 
+                        geno.file="MPP_Ms2_GWASPoly.txt", 
+                        format="ACGT", n.traits=length(trait1), delim=",")
+
+data_2 <- set.K(data = data_1, LOCO = F, n.core = 32)
+data_3 <- GWASpoly(data = data_2, models = models_1, traits = trait1, params = params, n.core = 30)
+data_5 <- set.threshold(data_3, method= "Bonferroni", level=0.05)
+QTL_01 <- get.QTL(data_5)
+QTL_02 <- QTL_01 %>% distinct(Marker, .keep_all = T) 
