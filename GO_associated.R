589f41e9fdf1583eb729cf0e82908cb9c6b50e0b
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-02-18 13:41:20 -0800

    add QTL_07 table; similar to QTL_10, but including GO terms.

--- GO_associated.R
+++ GO_associated.R
@@ -17,6 +17,7 @@ a2 <- inner_join(i_5.2.4, a1, by = "uniprot")  %>% dplyr::select(-1)
 head(a2)
 dim(a2)
 write.table(a2, '/home/hawkins/Documents/Cesar/RNA/globus/lordec_reports/lordec_trim/bed_Shen/ORF_NMD/background_3.tsv', sep = '\t', col.names = F, row.names = F, quote = F)
+a2 <- read.table('/home/hawkins/Documents/Cesar/RNA/globus/lordec_reports/lordec_trim/bed_Shen/ORF_NMD/background_3.tsv', header = F, sep = '\t')
 
 ##########################
 # this part of the code allows to generate QTL_10 table
@@ -33,8 +34,12 @@ head(QTL_10)
 ##########################
 # this part of code is the GO enrichment
 
+GOesByID.1 <- readMappings(file = '/home/hawkins/Documents/Cesar/RNA/globus/lordec_reports/lordec_trim/bed_Shen/ORF_NMD/background_4.tab')
 GOesByID.2 <- readMappings(file = '/home/hawkins/Documents/Cesar/RNA/globus/lordec_reports/lordec_trim/bed_Shen/ORF_NMD/background_3.tsv')
+
+head(GOesByID.1)
 head(GOesByID.2)
+bg_genes <- names(GOesByID.1)
 bg_genes <- names(GOesByID.2)
 bg_genes[1:5]
 
@@ -48,15 +53,38 @@ myGOdata_CC <- new("topGOdata", description="My project", ontology="CC", allGene
 
 myGOdata_MF <- new("topGOdata", description="My project", ontology="MF", allGenes=compared_genes, geneSel = topDiffGenes, nodeSize = 10, annot = annFUN.gene2GO, gene2GO = GOesByID.2)
 
-resultFisher_BP <- runTest(myGOdata_BP, algorithm = "weight01", statistic = "ks")
-resultFisher_CC <- runTest(myGOdata_CC, algorithm = "weight01", statistic = "ks")
-resultFisher_MF <- runTest(myGOdata_MF, algorithm = "weight01", statistic = "ks")
+resultFisher_BP <- runTest(myGOdata_BP, algorithm = "weight01", statistic = "fisher")
+resultFisher_CC <- runTest(myGOdata_CC, algorithm = "weight01", statistic = "fisher")
+resultFisher_MF <- runTest(myGOdata_MF, algorithm = "weight01", statistic = "fisher")
 
 tab_BP <- GenTable(myGOdata_BP, raw.p.value = resultFisher_BP, numChar = 120, topNodes = 20)
-tab_CC <- GenTable(myGOdata_CC, raw.p.value = resultFisher_CC, numChar = 120, topNodes = 10)
-tab_MF <- GenTable(myGOdata_MF, raw.p.value = resultFisher_MF, numChar = 120, topNodes = 10)
+tab_CC <- GenTable(myGOdata_CC, raw.p.value = resultFisher_CC, numChar = 120, topNodes = 20)
+tab_MF <- GenTable(myGOdata_MF, raw.p.value = resultFisher_MF, numChar = 120, topNodes = 40)
+
+tab_BP <- subset(tab_BP, Significant > 0)
+tab_CC <- subset(tab_CC, Significant > 0)
+tab_MF <- subset(tab_MF, Significant > 0)
 
 tab_BP$GO_process <- "BP"
 tab_CC$GO_process <- "CC"
 tab_MF$GO_process <- "MF"
+
+myterms =tab_MF$GO.ID
+mygenes = genesInTerm(myGOdata_MF, myterms)
+
+tab_MF$genes <- sapply(tab_MF$GO.ID, function(x)
+{
+  genes <- genesInTerm(myGOdata_MF, x) 
+  genes[[1]][genes[[1]] %in% sigGenes(myGOdata_MF)] # significant genes
+})
+
 GO_associated <- rbind(tab_BP, tab_CC, tab_MF)
+
+a1 <- unnest(GO_associated, genes) %>% dplyr::select(1,2,7,8)
+head(a1)
+colnames(a1)[4] <- "uniprot"
+QTL_11 <- a1 %>% group_by(uniprot) %>% summarise(GO.ID = paste(GO.ID, collapse = ";"))
+head(QTL_10)
+QTL_07 <- left_join(QTL_10, QTL_11, by = "uniprot")
+
+write.table(QTL_07, "~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/QTL_07.tsv", row.names = F, quote = F, sep = "\t")
