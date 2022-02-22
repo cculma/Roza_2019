f93db7f854b534918ce2e93a87478032ec2f4d35
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-02-22 14:15:42 -0800

    ld 1

--- ld_plot.R
+++ ld_plot.R
@@ -9,12 +9,12 @@ library(ggplot2)
 
 # load("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/GWASpoly_results/data_3_80177_year.RData")
 load("data_3_80177_year.RData")
-load("~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/myplot1.RData")
-myplot1 <- LD.plot(data_5) + theme_classic(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
-myplot2 <- myplot1 + theme_classic(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
+load("~/Documents/Cesar/blup_data/Roza2019/Analysis_2021/GWAS/myplot1.RData")
+myplot1 <- LD.plot(data_5) + theme_light(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
+myplot2 <- myplot1 + theme_light(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
 
 
-ggsave(filename = "~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/myplot1.pdf", plot = myplot1, width = 6, height = 6)
+ggsave(filename = "~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/myplot2.pdf", plot = myplot2, width = 3, height = 3)
 
 ggsave(filename = "myplot1.jpg", plot = myplot1, width = 6, height = 6)
 save(myplot1, file = "myplot1.RData")
