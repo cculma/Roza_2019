47923d4be43fe62425bcaff2fa05673c79677430
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-02-22 13:01:14 -0800

    add ld plot

--- job_ld_plot.sh
+++ job_ld_plot.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=gwas			# Job Name
 #SBATCH --output=gwas.out
 #SBATCH --error=gwas.err
-#SBATCH --time=0-06:00:00		# Wall clock time limit in Days-HH:MM:SS
-#SBATCH --nodes=2			# Node count required for the job
+#SBATCH --time=0-02:00:00		# Wall clock time limit in Days-HH:MM:SS
+#SBATCH --nodes=4			# Node count required for the job
 #SBATCH --ntasks-per-node=1		# Number of tasks to be launched per Node
 #SBATCH --cpus-per-task=20		# Number of threads per task (OMP threads)
 #SBATCH --mem=220GB			# Amount of memory per node
--- ld_plot.R
+++ ld_plot.R
@@ -9,8 +9,13 @@ library(ggplot2)
 
 # load("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/GWASpoly_results/data_3_80177_year.RData")
 load("data_3_80177_year.RData")
-
+load("~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/myplot1.RData")
 myplot1 <- LD.plot(data_5) + theme_classic(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
+myplot2 <- myplot1 + theme_classic(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
+
+
+ggsave(filename = "~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/myplot1.pdf", plot = myplot1, width = 6, height = 6)
 
 ggsave(filename = "myplot1.jpg", plot = myplot1, width = 6, height = 6)
+save(myplot1, file = "myplot1.RData")
 save(myplot1, file = "myplot1.RData")
