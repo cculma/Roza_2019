d372b22aa07807cddb9a686d19b9324cc5ebb2ea
Cesar A Medina <62046254+cculma@users.noreply.github.com>
2022-02-22 13:14:30 -0800

    theme light

--- ld_plot.R
+++ ld_plot.R
@@ -10,8 +10,9 @@ library(ggplot2)
 # load("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/GWASpoly_results/data_3_80177_year.RData")
 load("data_3_80177_year.RData")
 load("~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/myplot1.RData")
-myplot1 <- LD.plot(data_5) + theme_classic(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
-myplot2 <- myplot1 + theme_classic(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
+
+myplot1 <- LD.plot(data_5) + theme_classic(base_family = "Arial", base_size = 12) + ggtitle("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/")
+myplot2 <- myplot1 + theme_light(base_family = "Arial", base_size = 12) + ggtitle("LD plot")
 
 
 ggsave(filename = "~/Documents/Cesar/blup_data/Roza2019/git/Roza2019/myplot1.pdf", plot = myplot1, width = 6, height = 6)
