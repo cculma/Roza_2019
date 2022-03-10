bdbf1b681ce42d9c4743b1603bcbd5308e407816
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-03-10 13:14:41 -0800

    auto1

--- autoregressive1.R
+++ autoregressive1.R
@@ -1,25 +1,34 @@
 library(ggplot2)
 library(tidyverse)
-load("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/GWASpoly_results/data_3_80177_year.RData")
-colnames(pheno)
+library(asreml)
+
+# load("~/OneDrive - Washington State University (email.wsu.edu)/Roza_2019/GWASpoly_results/data_3_80177_year.RData")
+
 
 # may 1
 # jun 2
 # jul 3
 # aug 4
 # sep 5
+pheno <- read.csv("~/Documents/Cesar/blup_data/Roza2019/Analysis_2021/GWAS/pheno_nph.csv", row.names = 1)
+head(pheno)
+p1 <- pheno[1:(length(colnames(pheno))-3)]
 
-p1 <- pheno %>% dplyr::select(-c(24:26)) %>% rownames_to_column(var = "gen")
+p1 <- p1 %>% rownames_to_column(var = "gen")
 colnames(p1)
 p2 <- p1[,c(1:11)]
 
 colnames(p2) <-  c("gen", "ST1_Yi_2020_1","ST1_Yi_2020_2", "ST1_Yi_2020_3", "ST1_Yi_2020_4", "ST1_Yi_2020_5", "ST1_Yi_2021_1", "ST1_Yi_2021_2", "ST1_Yi_2021_3", "ST1_Yi_2021_4", "ST1_Yi_2021_5")
 p2 <- p2 %>% gather(key = "trait1", value = "BLUE", 2:11) %>% separate(2, into = c("stage", "trait", "year", "cut"), sep = "_", remove = F, convert = T, extra = "warn")
+head(p2)
+str(p2)
+L2 <- colnames(p2)[1:(length(colnames(p2))-1)]
+p2[,L2] <- lapply(p2[,L2], factor)
 
-ggplot(data = p2, aes(x = cut, y = BLUE, group = gen, color = gen)) + geom_line() + facet_grid(. ~ year) + theme(legend.position = "none")
+# ggplot(data = p2, aes(x = cut, y = BLUE, group = gen, color = gen)) + geom_line() + facet_grid(. ~ year) + theme(legend.position = "none")
 
 # variance model for the residual term
-model_uniform <- asreml(BLU3 ~ Year + cut + Year:cut,
+model_uniform <- asreml(BLUE ~ year*cut,
                         residual = ~gen:cor(cut), data = p2)
 
 summary(model_uniform)$bic
@@ -27,7 +36,7 @@ summary(model_uniform)$bic
 # the first autoregressive model (ar1) is the simplest model that allows correlations between pairs of measurements t decrease as the time between them increases.
 # ar1 can only be fitted to equally spaced measurements.
 
-# antedepence model
+# antedependence model
 model_ante <- asreml(BLU3 ~ Year + cut + Year:cut,
                         residual = ~gen:ante(cut), data = p2)
 summary(model_ante)$bic
