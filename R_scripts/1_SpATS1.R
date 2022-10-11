7867bff6c671dabc404f8be2f803b17443846594
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-10-11 15:41:01 -0700

    BLUE

--- R_scripts/1_SpATS1.R
+++ R_scripts/1_SpATS1.R
@@ -9,17 +9,12 @@ library(asreml)
 library(asremlPlus)
 library(patchwork)
 
-remotes::install_github("AparicioJohan/MrBeanApp")
-devtools::install_github("AparicioJohan/MrBeanApp", force = T)
-install.packages( pkgs = "summarytools" )
-
-library(MrBean)
-run_app()
 ############
 
 
 a1 <- read.csv("~/Documents/git/Roza_2019/raw_data/Roza2019_yield.csv")
 head(a1)
+
 colnames(a1)
 lev2 <- c("acc","acc_num","rep")
 a1[,lev2] <- lapply(a1[,lev2], factor)
@@ -33,7 +28,6 @@ nlevels(a1$R)
 
 lev3 <- colnames(a1)[7:23]
 
-
 Y1 <- list()
 for (i in 1:length(lev3)) {
   m1 <- SpATS(response = lev3[i],
@@ -45,7 +39,7 @@ for (i in 1:length(lev3)) {
               data = a1,
               control = list(tolerance = 1e-03))
   
-  pred4 <- predict.SpATS(m1, which = "acc_num", predFixed = "marginal")
+  pred4 <- predict.SpATS(m1, which = "acc_num", predFixed = "conditional")
   pred4 <- pred4[,c(1,7,8)]
   pred4$weight <- (1/pred4$standard.errors)^2
   Y1[[length(Y1)+1]] <- pred4
@@ -53,30 +47,10 @@ for (i in 1:length(lev3)) {
 names(Y1) <- lev3
 
 Y2 <-rbindlist(Y1, use.names=TRUE, fill=TRUE, idcol="env")
-colnames(Y2)[2] <- "gen" 
 head(Y2)
-
-Y3 <- Y2 %>% arrange(env) %>% mutate(env = factor(env, levels= lev3)) %>% select(1:3) %>% spread(key = env, value = predicted.values) %>% column_to_rownames("gen")
-
-Y3 <- ST1 %>% unite("env", c(year, month), sep = "_", remove = F) %>% select(c(1,2,5)) %>% spread(key = env, value = predicted.value) %>% column_to_rownames("gen")
-
-
-Y3 <- cor(Y3, use = "complete")
-
-# ggplot(Y3, aes(x = env, y = predicted.values)) + geom_boxplot(outlier.shape = NA, alpha = 0.6, width=0.6, position = position_dodge(width=0.8, preserve = "single")) + theme_bw(base_family = "Arial") + theme(legend.position = "none", panel.spacing = unit(0.3, "lines"), strip.text.x = element_text(size = 10), axis.text.x = element_text(angle = 90, hjust = 0.95, vjust = 0.2), axis.title = element_text(size = 12)) + labs(title = "Roza2019 yield ST10", y = "", x = "") + ylim(0, 400)
-
-
-# save.image("~/Documents/git/big_files/SpATS1.RData")
-# load("~/Documents/git/big_files/SpATS1.RData")
-#####################
-# ST1 FA1
-head(Y2)
-
+colnames(Y2)[2] <- "gen"
 Y2$env <- as.factor(Y2$env)
 levels(Y2$env)
-
-
-# Y4 <- Y2 %>% dplyr::filter(env %in% c("total_20","total_21","total_22"))
 
 Y4 <- Y2 %>% dplyr::filter(!env %in% c("total_20","total_21","total_22"))
 Y4$env <- droplevels(Y4$env)
@@ -87,41 +61,28 @@ lev5 <- c("month", "year")
 Y4 <- as.data.frame(Y4)
 Y4[,lev5] <- lapply(Y4[,lev5], factor)
 Y4$month <- fct_relevel(Y4$month, c("may", "jun", "jul", "aug", "sep"))
+str(Y4)
+
 levels(Y4$month)
 levels(Y4$year)
+levels(Y4$gen)
 
 data <- Y4
-data <- data[order(data$gen, data$env), ]
+data <- data[order(data$gen, data$year), ]
 data1 <- na.omit(data)
 head(data1)
 str(data1)
 
 FA_1 <- asreml::asreml(fixed = predicted.values ~ 1, 
-                       random = ~ fa(year, 1):ar1(month):id(gen) + env,
+                       random = ~ sfa(year, 1):ar1(month):id(gen) + env,
                        data = data1, na.action = list(x = "include", y = "include"), 
                        weights = weight, family = asreml::asr_gaussian(dispersion = 1))
 
-# model <- asreml(fixed = yield ~ 1 + county,
-#                 random = ~ fa(county, 2):gen + county:rep + diag(county):rep:block,
-#                 residual = ~ dsum(~ units | county),
-#                 data = dat,
-#                 na.action = list(x="include",y="include"))
 
 FA_1 <- update.asreml(FA_1)
 summary(FA_1)$varcomp
 current.asrt <- as.asrtests(FA_1, NULL, NULL)
 current.asrt <- rmboundary.asrtests(current.asrt)
-
-
-ST1 <- predict.asreml(FA_1, classify='gen:env', vcov=F)$pvals
-head(ST1)
-hist(ST1$predicted.value)
-
-# this part collapse the program
-# diffs <- predictPlus(classify = "gen:env", 
-#                      asreml.obj = FA_1, 
-#                      wald.tab = current.asrt$wald.tab, 
-#                      present = c("month","gen","year","env"))
 
 
 diffs <- predictPlus(classify = "gen:month", 
@@ -142,47 +103,78 @@ diffs <- predictPlus(classify = "gen",
                      present = c("month","gen","year","env"))
 ST4 <-diffs[[1]]
 
+ST1 <- predict.asreml(FA_1, classify='gen:env', vcov=F)$pvals
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ST2$month <- paste0("ST2_", ST2$month)
+ST3$year <- paste0("ST3_", ST3$year)
+
+colnames(ST4)[2] <- "ST4_Yi"
+ST4 <- ST4 %>% select(1:2)
+
+ST2 <- ST2 %>% select(1:3) %>% spread(key = month, value = predicted.value) 
+ST3 <- ST3 %>% select(1:3) %>% spread(key = year, value = predicted.value)
+ST2 <- ST2[,c(1,5,4,3,2,6)]
+
+head(Y2)
+Y2.1 <- Y2
+Y2.1$env <- paste0("ST0_", Y2.1$env)
+Y2.1$env <- as.factor(Y2.1$env)
+levels(Y2.1$env)
+ST0 <- Y2.1 %>% select(1:3) %>% spread(key = env, value = predicted.values)
+
+
 head(ST1)
-head(ST2)
-head(ST3)
-head(ST4)
+ST1$env <- paste0("ST1_", ST1$env)
+ST1 <- ST1 %>% select(1:3) %>% spread(key = env, value = predicted.value)
+
+BLUP5 <- inner_join(ST0, ST1, by = "gen") %>% inner_join(., ST2, by = "gen") %>% inner_join(., ST3, by = "gen")  %>% inner_join(., ST4, by = "gen")
+colnames(BLUP5)
+
+write.csv(BLUP5, "~/Documents/git/Roza_2019/pheno_data/BLUEs_Yi_Roza2019.csv", quote = F, row.names = F)
+
+#####################
+# ST1 FA1
+head(Y2)
+
+Y2$env <- as.factor(Y2$env)
+levels(Y2$env)
 
 
-hist(ST4$predicted.value)
+# Y4 <- Y2 %>% dplyr::filter(env %in% c("total_20","total_21","total_22"))
+
+Y4 <- Y2 %>% dplyr::filter(!env %in% c("total_20","total_21","total_22"))
+Y4$env <- droplevels(Y4$env)
+levels(Y4$env)
+head(Y4)
+Y4 <- Y4 %>% separate(1, c("month", "year"), sep = "_", remove = F, convert = FALSE, extra = "merge")
+lev5 <- c("month", "year")
+Y4 <- as.data.frame(Y4)
+Y4[,lev5] <- lapply(Y4[,lev5], factor)
+Y4$month <- fct_relevel(Y4$month, c("may", "jun", "jul", "aug", "sep"))
+str(Y4)
+Y4$gen <- as.factor(Y4$gen)
+levels(Y4$month)
+levels(Y4$year)
+
+data <- Y4
+data <- data[order(data$gen, data$env), ]
+data1 <- na.omit(data)
+head(data1)
+str(data1)
 
 
-US <- asreml::asreml(fixed = predicted.values ~ 1 + gen,
-                       random = ~ us(year):ar1(month):id(gen),
-                       data = data1, na.action = list(x = "include", y = "include"),
-                       weights = weight, family = asreml::asr_gaussian(dispersion = 1))
 
-
-summary(FA_1)$aic
-summary(US)$aic
-
-
-current.asrt <- as.asrtests(US, NULL, NULL)
+FA_1 <- update.asreml(FA_1)
+summary(FA_1)$varcomp
+current.asrt <- as.asrtests(FA_1, NULL, NULL)
 current.asrt <- rmboundary.asrtests(current.asrt)
 
-diffs <- predictPlus(classify = "gen:year:month", 
-                     asreml.obj = US, 
-                     wald.tab = current.asrt$wald.tab, 
-                     present = c("month","gen","year"))
 
 
-diffs <- predictPlus(classify = "gen:month", 
-                     asreml.obj = US, 
-                     wald.tab = current.asrt$wald.tab, 
-                     present = c("month","gen","year"))
-ST2 <-diffs[[1]]
 
-diffs <- predictPlus(classify = "gen:year", 
-                     asreml.obj = US, 
-                     wald.tab = current.asrt$wald.tab, 
-                     present = c("month","gen","year"))
-
-names(diffs)
-ST3 <- diffs[[1]]
 
 
 
@@ -190,19 +182,30 @@ ST3 <- diffs[[1]]
 # asreml.options(workspace="512mb")
 # asreml.options(workspace="1024mb")
 
-# save.image("~/Documents/git/big_files/1_SpATS1.RData")
-# load("~/Documents/git/big_files/1_SpATS1.RData")
 
-ST1 <- predict.asreml(US, classify='gen:year:month', vcov=F)$pvals
-ST2 <- predict.asreml(US, classify='gen:month', vcov=F)$pvals
-ST3 <- predict.asreml(US, classify='gen:year', vcov=F)$pvals
-ST4 <- predict.asreml(US, classify='gen', vcov=F)$pvals
 
-hist(ST1$predicted.value)
-hist(ST2$predicted.value)
-hist(ST3$predicted.value)
-hist(ST4$predicted.value)
 
+# ~~~~~~~~~~~~~~~~~~~~~
+
+# Y2 <- read.csv("~/Documents/git/Roza_2019/raw_data/BLUE_roza2019.csv")
+# colnames(Y2)[1:2] <- c("env","gen") 
+head(Y2)
+
+
+Y3 <- Y2 %>% arrange(env) %>% mutate(env = factor(env, levels= lev3)) %>% select(1:3) %>% spread(key = env, value = predicted.values) %>% column_to_rownames("gen")
+
+Y3 <- ST1 %>% unite("env", c(year, month), sep = "_", remove = F) %>% select(c(1,2,5)) %>% spread(key = env, value = predicted.value) %>% column_to_rownames("gen")
+
+
+Y3 <- cor(Y3, use = "complete")
+
+# ggplot(Y3, aes(x = env, y = predicted.values)) + geom_boxplot(outlier.shape = NA, alpha = 0.6, width=0.6, position = position_dodge(width=0.8, preserve = "single")) + theme_bw(base_family = "Arial") + theme(legend.position = "none", panel.spacing = unit(0.3, "lines"), strip.text.x = element_text(size = 10), axis.text.x = element_text(angle = 90, hjust = 0.95, vjust = 0.2), axis.title = element_text(size = 12)) + labs(title = "Roza2019 yield ST10", y = "", x = "") + ylim(0, 400)
+
+
+save.image("~/Documents/git/big_files/SpATS1.RData")
+load("~/Documents/git/big_files/SpATS1.RData")
+
+# plots
 
 
 # BLUP2 <- BLUP2 %>% arrange(env) %>% mutate(env = factor(env, levels= lev4))
