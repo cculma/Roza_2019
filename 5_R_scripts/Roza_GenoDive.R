0e7a50b8f5d6310714464a64aacade854556fb08
cesar <medin297@umn.edu>
2024-11-14 14:17:28 -0600

    GenoDive

--- 5_R_scripts/Roza_GenoDive.R
+++ 5_R_scripts/Roza_GenoDive.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 
 setwd("~/Documents/git/big_files/")
 
@@ -7,21 +7,22 @@ head(PCA)
 
 PCA1 <- PCA %>% dplyr::select( Susceptible_parent1, Roza2019_VCF_ID)
 head(PCA1)
-# 
-# PCA$pop2 <- recode_factor(PCA$Susceptible_parent, 
-#                           "1613-1"  = "1", "1613-10" = "2", "1613-11" = "3", 
-#                           "1613-12" = "4", "1613-13" = "5", "1613-14" = "6",
-#                           "1613-15" = "7", "1613-16" = "8", "1613-17" = "9",
-#                           "1613-18" = "10", "1613-19" = "11", "1613-2" = "12",
-#                           "1613-20" = "13", "1613-21" = "14", "1613-22" = "15",
-#                           "1613-23" = "16", "1613-25" = "17", "1613-26" = "18", 
-#                           "1613-28" = "19", "1613-29" = "20", "1613-3" = "21", 
-#                           "1613-30" "1613-31" "1613-32" , 
-#                           "1613-4"  "1613-5" "1613-6" ,
-#                           "1613-7"  "1613-8"  "1613-9" )
+PCA1$Susceptible_parent1 <- paste0("P",PCA1$Susceptible_parent1)
+
+
+ 
+# PCA$pop2 <- recode_factor(PCA$Susceptible_parent1, 
+#                           "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8","P9",
+#                           "P10", "P11", "P12", "P13", "P14", "P15", "P16","P17",
+#                           "P18", "P19", "P20", "P21", "P22", "P23", "P24", "P25", "P26", "P27", "P28",
+#                           "P29", "P30")
                           
 cc06 <- dplyr::count(PCA1, Susceptible_parent1)
 cc06$Susceptible_parent1
+cc06$seq <- seq(1:30)
+cc07 <- inner_join(cc06, PCA1, by = "Susceptible_parent1")
+cc07 <- cc07 %>% dplyr::select(seq, Roza2019_VCF_ID)
+
 
 G1 <- read.table("Roza2019_06_GS.txt", sep = "\t", row.names = 1)
 G1[1:5,1:5]
@@ -39,8 +40,9 @@ G1[G1 == "4"] <- "002002002002"
 df4 <- as.data.frame(G1)
 df4 <- df4 %>% rownames_to_column("Roza2019_VCF_ID")
 df4[1:5,1:5]
+dim(df4)
 
-col9.2 <- inner_join(PCA1, df4, by = "Roza2019_VCF_ID")
+col9.2 <- inner_join(cc07, df4, by = "Roza2019_VCF_ID")
 colnames(col9.2)[1] <- "pop"
 colnames(col9.2)[2] <- "ind"
 col9.2[1:10, 1:5]
